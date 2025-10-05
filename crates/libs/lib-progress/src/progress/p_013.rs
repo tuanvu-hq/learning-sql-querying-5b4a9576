3d1f173e9d65362d4d93162614e07ee87bceee8b
Tuan Vu Hoang Quang <tuanvu.hoangquang@proton.me>
2025-10-05 15:53:19 +0200

    feat: add progress

---
+++ crates/libs/lib-progress/src/progress/p_013.rs
@@ -0,0 +1,90 @@
+use polars::prelude::*;
+use sea_orm::{DatabaseConnection, EntityTrait, FromQueryResult, QuerySelect};
+use sqlx::{FromRow, Pool, Postgres};
+
+use lib_core::error::{AppError, AppResult};
+use lib_data::database::customers;
+
+use crate::model::df_customers::df_customers;
+use crate::utils::compare::compare_vecs;
+use crate::utils::database::get_database;
+use crate::utils::debug::log_debug;
+
+/*
+# QUERY:
+
+SELECT DISTINCT
+    country
+FROM customers;
+*/
+
+/*
+shape: (3, 1)
+┌─────────┐
+│ country │
+│ ---     │
+│ str     │
+╞═════════╡
+│ USA     │
+│ Germany │
+│ UK      │
+└─────────┘
+*/
+
+const DEBUG: bool = false;
+
+#[derive(Clone, Debug, PartialEq, FromRow, FromQueryResult)]
+struct Customer {
+    country: Option<String>,
+}
+
+async fn sea_orm_query(db: &DatabaseConnection) -> AppResult<Vec<Customer>> {
+    let results = customers::Entity::find()
+        .select_only()
+        .column(customers::Column::Country)
+        .distinct()
+        .into_model::<Customer>()
+        .all(db)
+        .await
+        .map_err(AppError::SeaOrm)?;
+
+    log_debug("SEA ORM", &results, Some(DEBUG));
+
+    Ok(results)
+}
+
+async fn sqlx_query(db: &Pool<Postgres>) -> AppResult<Vec<Customer>> {
+    let query = "
+    SELECT DISTINCT
+        country
+    FROM customers;
+    ";
+    let results = sqlx::query_as::<_, Customer>(query)
+        .fetch_all(db)
+        .await
+        .map_err(AppError::Sqlx)?;
+
+    log_debug("SQLX", &results, Some(DEBUG));
+
+    Ok(results)
+}
+
+pub async fn display_table() -> AppResult<()> {
+    let (db_sea_orm, db_sqlx) = get_database().await?;
+    let df = df_customers(&db_sea_orm)
+        .await?
+        .lazy()
+        .select([col("country")])
+        .unique(None, UniqueKeepStrategy::First)
+        .collect()
+        .map_err(AppError::Polars)?;
+
+    if compare_vecs(
+        &sea_orm_query(&db_sea_orm).await?,
+        &sqlx_query(&db_sqlx).await?,
+    ) {
+        log_debug("POLARS", &df, None);
+    }
+
+    Ok(())
+}
---
+++ crates/libs/lib-progress/src/progress/p_018.rs
@@ -0,0 +1,80 @@
+use polars::prelude::*;
+use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
+use sqlx::{Pool, Postgres};
+
+use lib_core::error::{AppError, AppResult};
+use lib_data::database::customers;
+
+use crate::model::df_customers::df_customers;
+use crate::utils::compare::compare_vecs;
+use crate::utils::database::get_database;
+use crate::utils::debug::log_debug;
+
+/*
+# QUERY:
+
+SELECT *
+FROM customers
+WHERE score > 500;
+*/
+
+/*
+shape: (2, 4)
+┌─────┬────────────┬─────────┬───────┐
+│ id  ┆ first_name ┆ country ┆ score │
+│ --- ┆ ---        ┆ ---     ┆ ---   │
+│ i32 ┆ str        ┆ str     ┆ i32   │
+╞═════╪════════════╪═════════╪═══════╡
+│ 2   ┆  John      ┆ USA     ┆ 900   │
+│ 3   ┆ Georg      ┆ UK      ┆ 750   │
+└─────┴────────────┴─────────┴───────┘
+*/
+
+const DEBUG: bool = false;
+
+async fn sea_orm_query(db: &DatabaseConnection) -> AppResult<Vec<customers::Model>> {
+    let results = customers::Entity::find()
+        .filter(customers::Column::Score.gt(500))
+        .all(db)
+        .await
+        .map_err(AppError::SeaOrm)?;
+
+    log_debug("SEA ORM", &results, Some(DEBUG));
+
+    Ok(results)
+}
+
+async fn sqlx_query(db: &Pool<Postgres>) -> AppResult<Vec<customers::Model>> {
+    let query = "
+    SELECT *
+    FROM customers
+    WHERE score > 500;
+    ";
+    let results = sqlx::query_as::<_, customers::Model>(query)
+        .fetch_all(db)
+        .await
+        .map_err(AppError::Sqlx)?;
+
+    log_debug("SQLX", &results, Some(DEBUG));
+
+    Ok(results)
+}
+
+pub async fn display_table() -> AppResult<()> {
+    let (db_sea_orm, db_sqlx) = get_database().await?;
+    let df = df_customers(&db_sea_orm)
+        .await?
+        .lazy()
+        .filter(col("score").gt(500))
+        .collect()
+        .map_err(AppError::Polars)?;
+
+    if compare_vecs(
+        &sea_orm_query(&db_sea_orm).await?,
+        &sqlx_query(&db_sqlx).await?,
+    ) {
+        log_debug("POLARS", &df, None);
+    }
+
+    Ok(())
+}
